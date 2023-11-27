b34c8e623540e145d3086c5ff6abf752a0a46a93
BTorija <beatriz_torija@hotmail.com>
2023-11-27 19:47:06 +0100

    Initial commit

---
+++ src/Ejercicio01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static <print> void main(String[] args) throws Exception {
+
+        // Realizar la raíz cuadrada de 225 en un procedimiento main.
+
+        System.out.println("Calcular la raiz cuadrada de 225");
+        int numero = 225;
+     long resultado = (long) Math.sqrt(numero);
+        System.out.println("La raiz cuadrada de "+numero+" es: "+resultado);
+
+    }
+
+
+}
---
+++ src/Ejercicio02.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        //Realizar un procedimiento main que realice el promedio de 5 números que se
+        //almacene en un array de números float y se muestren sus datos.
+        System.out.println("Realizar el promedio de 5 numeros ");
+        float numero1=5;
+        float numero2=6;
+        float numero3=7;
+        float numero4=8;
+        float numero5=9;
+    }
+}
